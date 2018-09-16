4b5a76a3c709a8015a410763a172e2308bb4ba99
quilicicf <cquilici@restlet.com>
2018-09-16 19:19:29 +0200

    :up: Add icons to the themes

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 
 const _ = require('lodash');
 const deepmerge = require('deepmerge');
-const { writeFileSync, mkdirSync, existsSync } = require('fs');
+const bufferReplace = require('buffer-replace');
 const { execSync } = require('child_process');
 const { resolve } = require('path');
+const svg2png = require('svg2png');
+const { readFile, writeFile } = require('pn/fs');
+const { writeFileSync, mkdirSync, existsSync } = require('fs');
 
 const package = require('./package');
 
@@ -12,6 +15,8 @@ const { VARIATIONS } = require('./lib/variations');
 const manifestTemplate = require('./lib/manifest-template');
 
 const DIST_FOLDER = resolve(__dirname, 'dist');
+const RAW_ICON_FOLDER = resolve(__dirname, 'lib', 'material_design_camera.svg');
+const ICONS_SIZES = [ 16, 48, 128 ];
 
 const generateManifest = (manifestPatcher) => (
   deepmerge.all([
@@ -21,6 +26,22 @@ const generateManifest = (manifestPatcher) => (
   ])
 );
 
+const generateIcons = async (colorHex, targetFolder) => {
+  const iconsPromises = _.map(ICONS_SIZES, (size) => (
+    readFile(RAW_ICON_FOLDER)
+      .then(svgIconBuffer => {
+        const replacedBuffer = bufferReplace(svgIconBuffer, '#000000', colorHex);
+        return svg2png(replacedBuffer, { width: size, height: size });
+      })
+      .then(pngIconBuffer => writeFile(resolve(targetFolder, `icon-${size}.png`), pngIconBuffer))
+      .catch(error => {
+        throw error;
+      })
+  ));
+
+  return Promise.all(iconsPromises);
+};
+
 const mkdirIfAbsent = (path) => {
   if (existsSync(path)) {
     return;
@@ -29,16 +50,22 @@ const mkdirIfAbsent = (path) => {
   mkdirSync(path);
 };
 
-const writeInDist = ({ colorName, manifest }) => {
+const writeInDist = async ({ colorName, colorHex, manifest }) => {
   const targetFolder = resolve(DIST_FOLDER, `chrome-sober-${manifest.colorName}-theme`);
   execSync(`rm -rf ${targetFolder}`);
   mkdirSync(targetFolder);
 
-  const targetPath = resolve(targetFolder, 'manifest.json');
-  writeFileSync(targetPath, JSON.stringify(manifest, null, 2), 'utf8');
+  const targetManifestPath = resolve(targetFolder, 'manifest.json');
+  writeFileSync(targetManifestPath, JSON.stringify(manifest, null, 2), 'utf8');
+
+  await generateIcons(colorHex, targetFolder);
 };
 
 mkdirIfAbsent(DIST_FOLDER);
 _(VARIATIONS)
-  .map(variation => ({ colorName: variation.colorName, manifest: generateManifest(variation.manifestPatcher) }))
-  .each(manifestAsJsObject => writeInDist(manifestAsJsObject));
+  .map(variation => ({
+    colorName: variation.colorName,
+    colorHex: variation.colorHex,
+    manifest: generateManifest(variation.manifestPatcher),
+  }))
+  .each(builtVariation => writeInDist(builtVariation));
